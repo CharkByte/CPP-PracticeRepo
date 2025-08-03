0a805e6b2d235fbb51311c539bf6c2fa468f6dc4
Bryant Galo <CharkByteDev@gmail.com>
2025-08-02 23:02:26 -0700

    SumOfTwoNumbers
    A basic program, declaring and initializing two variables, summing them together into a total variable.

---
+++ SumOfTwoNumbers.cpp
@@ -0,0 +1,20 @@
+// C++ Practice. Basics to advanced topics
+/*
+	Sum of two Numbers program: 
+    
+    A program that stores integers 50 and 100 in variables
+    and stores the sum of these two in a variable named total
+*/
+
+
+#include <iostream> 
+using namespace std;
+int main()
+{
+    int var1, var2, total;
+
+    var1 = 50, var2 = 100;
+    total = var1 + var2;
+
+    return 0;
+}
