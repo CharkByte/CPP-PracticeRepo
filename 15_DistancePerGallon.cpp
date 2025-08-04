13636d1ec3fbbb5c9ae7af4aac484be4a517eccd
Bryant Galo <CharkByteDev@gmail.com>
2025-08-03 17:08:24 -0700

    Add files via upload

---
+++ 15_DistancePerGallon.cpp
@@ -0,0 +1,20 @@
+/*
+	Distance per gallon program
+
+	This program will calculate the total distance a car can drive on a gallon.
+	The car can hold 20 gallons, and can travel 28.9 miles per gallon.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	double gallons = 20.0, miles, MPG = 28.9;
+
+	miles = MPG * gallons;
+
+	cout << "The amount of miles per one gallon this car can drive is: "
+		<< miles << " miles.";
+
+	return 0;
+}
