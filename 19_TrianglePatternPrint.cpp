2fc23c26df594d5d5ab871ffea5fe9992df4b6da
Bryant Galo <CharkByteDev@gmail.com>
2025-08-04 11:56:35 -0700

    Add files via upload

---
+++ 19_TrianglePatternPrint.cpp
@@ -0,0 +1,18 @@
+/*
+	Triangle pattern printout  program
+
+	This program will print out a pyramid of star symbols *, 
+	with the lowest tier being 7 stars.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	cout << "   *   \n";
+	cout << "  ***   \n";
+	cout << " ***** \n";
+	cout << "*******\n";
+	return 0;
+}
