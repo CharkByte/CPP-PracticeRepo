ea08f896725b1625268208740ba977c5224fb941
Bryant Galo <CharkByteDev@gmail.com>
2025-08-04 11:58:37 -0700

    Add files via upload

---
+++ 20_DiamondPatternPrint.cpp
@@ -0,0 +1,21 @@
+/*
+	Diamond pattern printout program
+
+	This program will print out a diamond of star symbols *, 
+	with the middle tier being 7 stars.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	cout << "   *   \n";
+	cout << "  ***   \n";
+	cout << " ***** \n";
+	cout << "*******\n";
+	cout << " ***** \n";
+	cout << "  ***   \n";
+	cout << "   *   \n";
+	return 0;
+}
